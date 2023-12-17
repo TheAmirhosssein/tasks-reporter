8b9aaede6580579bacf302c0b751ae4e2721bcc2
amirhossein <amirhosseinmobini9@gmail.com>
2023-12-17 15:54:03 +0330

    tasks model created

--- task/models.py
+++ task/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+STATUS = (
+    ("S", "started"),
+    ("F", "finished"),
+)
+    
+
+class Day(models.Model):
+    day_title = models.CharField(max_length=50)
+    day_date = models.DateField()
+    status = models.CharField(choices=STATUS, max_length=50)
+
+class Task(models.Model):
+    day_title = models.TextField()
+    started_time = models.TimeField(auto_now_add=True)
+    finished_time = models.TimeField()
+    status = models.CharField(choices=STATUS, max_length=50)
+    issue_link = models.URLField()
+    day = models.ForeignKey(Day, on_delete=models.CASCADE)
